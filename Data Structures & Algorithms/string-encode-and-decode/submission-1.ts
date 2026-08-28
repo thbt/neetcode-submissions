c47e6800fdae0cad5127c8a6c2eaa7b2d3ccbbfe
Thibaut Roy <thibaut.royhelion@gmail.com>
2026-08-28 18:24:44 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        if (strs.length == 0) { return '🪹'; }
+        return strs.join('🦆');
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        if (str === '🪹') { return []; }
+        return str.split('🦆');
+    }
+}
